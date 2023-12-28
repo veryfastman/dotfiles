c61875ed316a834103fe886b0d267e64a795620e
veryfastman <dony357@outlook.com>
2023-12-28 13:01:32 -0800

    commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,17 @@ if status is-interactive
 end
 
 set fish_greeting
-
 fish_add_path ~/.local/bin
-
+fish_add_path ~/.cargo/bin
 export VISUAL="nvim"
 export EDITOR="$VISUAL"
 
 alias v="nvim"
+alias cb="cargo build"
+alias cch="cargo check"
 alias cpptem="cp -r ~/Templates/programming/cpp_template"
 alias ctem="cp -r ~/Templates/programming/c_template"
+alias cv="cargo run"
 alias diro="ll | awk '(NR>1) { print $9 }'"
 alias fdh="cd \$(find . -type d -path '*/.*' -prune -o -not -name '.*' -type d -print | fzf)"
 alias fishrc="nvim ~/.config/fish/config.fish"
@@ -26,9 +28,9 @@ alias rem="sudo pacman -R"
 alias remall="sudo pacman -Rns"
 alias oph="pacman -Qtdq"
 alias se="sudoedit"
-alias snip="grim -g '\$(slurp)'"
+alias snip="grim -g \$(slurp)"
 alias tms="tmux new-session -c . -s"
-alias update="sudo pacman -Syu && yay -Syu"
+alias update="sudo pacman -Syu && paru -Syu"
 alias ytaudio="yt-dlp --extract-audio --audio-format mp3"
 alias ytvideo="yt-dlp -f 'bv*[height=1080]+ba'"
 
